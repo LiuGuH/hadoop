af5727476de59c540d0b5192bc77ccc8e8383e2a
Simba Dzinamarira <sdzinamarira@linkedin.com>
2022-07-18 09:42:13 -0700

    HDFS-16669: Enhance client protocol to propagate last seen state IDs for multiple nameservices.
    Fixes #4584
    
    Signed-off-by: Owen O'Malley <oomalley@linkedin.com>

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/ipc/Server.java
@@ -802,7 +802,7 @@ public abstract class Server {
     private volatile String detailedMetricsName = "";
     final int callId;            // the client's call id
     final int retryCount;        // the retry count of the call
-    long timestampNanos;         // time the call was received
+    private final long timestampNanos; // time the call was received
     long responseTimestampNanos; // time the call was served
     private AtomicInteger responseWaitCount = new AtomicInteger(1);
     final RPC.RpcKind rpcKind;
@@ -984,6 +984,10 @@ public abstract class Server {
 
     public void setDeferredError(Throwable t) {
     }
+
+    public long getTimestampNanos() {
+      return timestampNanos;
+    }
   }
 
   /** A RPC extended call queued for handling. */
@@ -1065,7 +1069,7 @@ public abstract class Server {
 
       try {
         value = call(
-            rpcKind, connection.protocolName, rpcRequest, timestampNanos, connection.getHostAddress(), connection.user.getShortUserName());
+            rpcKind, connection.protocolName, rpcRequest, getTimestampNanos(), connection.getHostAddress(), connection.user.getShortUserName());
       } catch (Throwable e) {
         populateResponseParamsOnError(e, responseParams);
       }
