bb109992c66b826fe648ca672cf33138f2ccc65c
lgh <liuguanghua@kanzhun.com>
2023-11-22 11:43:06 +0800

    [RBF] Fix listStatus trash path error when mount point with different nameservices

--- hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/resolver/MountTableResolver.java
+++ hadoop-hdfs-project/hadoop-hdfs-rbf/src/main/java/org/apache/hadoop/hdfs/server/federation/resolver/MountTableResolver.java
@@ -351,7 +351,7 @@ public class MountTableResolver
   @VisibleForTesting
   public static boolean isTrashPath(String path) throws IOException {
     Pattern pattern = Pattern.compile(
-        "^" + getTrashRoot() + TRASH_PATTERN + "/");
+        "^" + getTrashRoot() + TRASH_PATTERN);
     return pattern.matcher(path).find();
   }
 
